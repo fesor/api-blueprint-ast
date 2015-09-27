f47767e5c0a3eb3dd8edd501bf77026cec7d71e2
Sergey Protko <fesors@gmail.com>
2015-09-27 19:04:51 +0300

    Add blueprint builder

---
+++ src/BlueprintBuilder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Fesor\ApiBlueprint;
+
+use Fesor\ApiBlueprint\Element\Blueprint;
+
+class BlueprintBuilder
+{
+
+    private $blueprint;
+
+    /**
+     * @param Blueprint $blueprint
+     */
+    public function __construct(Blueprint $blueprint)
+    {
+        $this->blueprint = $blueprint;
+    }
+
+    /**
+     * @param string $name
+     * @param string $value
+     */
+    public function addMetadata($name, $value)
+    {
+        $this->blueprint->addMetadata($name, $value);
+    }
+
+}
