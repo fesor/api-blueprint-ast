ad81e62456932ff16a74f3ba735742299168903b
Sergey Protko <fesors@gmail.com>
2015-10-01 02:04:42 +0300

    Add blueprint builder context

---
+++ spec/BuilderContextSpec.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace spec\Fesor\ApiBlueprint;
+
+use Fesor\ApiBlueprint\Element\Blueprint;
+use Fesor\ApiBlueprint\Element\Category;
+use Fesor\ApiBlueprint\Element\Element;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class BuilderContextSpec extends ObjectBehavior
+{
+    private $blueprint;
+
+    function let(Blueprint $blueprint)
+    {
+        $this->blueprint = $blueprint;
+
+        $this->beConstructedWith($blueprint);
+    }
+
+    function it_handles_context()
+    {
+        $this->getContext()->shouldReturn($this->blueprint);
+    }
+
+    function it_holds_stack_of_passed_nodes(Element $category)
+    {
+        $this->pushContext($category);
+        $this->getContext()->shouldReturn($category);
+    }
+
+    function it_restores_context_to_previous_one(Element $category)
+    {
+        $this->pushContext($category);
+        $this->popContext()->shouldReturn($category);
+        $this->getContext()->shouldReturn($this->blueprint);
+    }
+
+    function it_restores_context_to_specific_node(Element $category)
+    {
+        $this->pushContext($category);
+        $this->shouldNotThrow()->duringRestoreContextTo($this->blueprint);
+        $this->getContext()->shouldReturn($this->blueprint);
+    }
+
+    function it_throws_exception_if_no_such_element_found_in_stack(Element $category)
+    {
+        $this->shouldThrow()->duringRestoreContextTo($category);
+    }
+
+    function it_restores_context_to_element_of_specific_type(Element $resource)
+    {
+        $resource->getElementType()->willReturn('resource');
+        $this->blueprint->getElementType()->willReturn('category');
+
+        $this->pushContext($resource);
+        $this->shouldNotThrow()->duringRestoreContextToElementOfType('category');
+        $this->getContext()->shouldReturn($this->blueprint);
+    }
+
+}
---
+++ src/BuilderContext.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Fesor\ApiBlueprint;
+
+use Fesor\ApiBlueprint\Element\Element;
+
+class BuilderContext
+{
+
+    /**
+     * @var Element
+     */
+    private $context;
+
+    /**
+     * @var Element[]
+     */
+    private $contextStack;
+
+    /**
+     * BuilderContext constructor.
+     * @param Element $context
+     */
+    public function __construct(Element $context)
+    {
+        $this->context = $context;
+        $this->contextStack = [];
+    }
+
+    /**
+     * @return Element
+     */
+    public function getContext()
+    {
+        return $this->context;
+    }
+
+    /**
+     * @param Element $context
+     */
+    public function pushContext(Element $context)
+    {
+        $this->contextStack[] = $this->context;
+        $this->context = $context;
+    }
+
+    /**
+     * @return Element
+     */
+    public function popContext()
+    {
+        $context =  $this->context;
+        $this->context = array_pop($this->contextStack);
+
+        return $context;
+    }
+
+    /**
+     * @param Element $element
+     */
+    public function restoreContextTo(Element $element)
+    {
+        while($this->context && $element !== $this->context) {
+            $this->popContext();
+        }
+
+        if (!$this->context) {
+            throw new \LogicException('Unable to restore context');
+        }
+    }
+
+    /**
+     * @param string $elementType
+     */
+    public function restoreContextToElementOfType($elementType)
+    {
+        while($this->context && $this->context->getElementType() !== $elementType) {
+            $this->popContext();
+        }
+
+        if (!$this->context) {
+            throw new \LogicException(
+                sprintf('Unable to restore context to element of type "%s"', $elementType)
+            );
+        }
+    }
+
+}
